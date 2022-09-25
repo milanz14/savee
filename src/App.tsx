6609f9eaf18531ec8d1c3542868f8a85df6e52f1
Milan Zagorac <zagoracm@gmail.com>
2022-09-25 15:01:00 -0400

    create placeholder landing page, adjust CSS for Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Landing from "./components/Landing";
 import Login from "./components/Login";
 import Register from "./components/Register";
 
@@ -7,7 +8,8 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </div>
