26912d7ba7bbcf561a05d77d01d3c98bdea60476
Milan Zagorac <zagoracm@gmail.com>
2022-09-29 09:38:19 -0400

    add firebase methods for auth, add LoginContext and wrap App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Landing from "./components/Landing";
 import Details from "./components/Details";
@@ -6,17 +7,22 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import ErrorPage from "./components/ErrorPage";
 
+import { LoginContext } from "./helpers/LoginContext";
+
 function App(): JSX.Element {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/details" element={<Details />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </div>
+    <LoginContext.Provider value={{ loggedIn, setLoggedIn }}>
+      <div>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </div>
+    </LoginContext.Provider>
   );
 }
 
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,14 @@ import firebase from "firebase";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -19,6 +26,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 export default app;
