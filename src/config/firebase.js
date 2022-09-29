4f687a127ab17b6234b8a506fb7fe134221f883a
Milan Zagorac <zagoracm@gmail.com>
2022-09-29 13:49:50 -0400

    change secrets, store in local env file, reference ENV variables in config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,32 +1,20 @@
 // npm i firebase
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  sendPasswordResetEmail,
-  signOut,
-} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyApsgeL5U0shbilwToTHkkIQoLURU2iRNA",
-  authDomain: "expenses-75d63.firebaseapp.com",
-  projectId: "expenses-75d63",
-  storageBucket: "expenses-75d63.appspot.com",
-  messagingSenderId: "850529543813",
-  appId: "1:850529543813:web:3e8bbc03354d40bd1e6bec",
-  measurementId: "G-QPLW6MHPWE",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MESASUREMENT_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { app, auth, db };
+const app = firebase.initializeApp(firebaseConfig);
