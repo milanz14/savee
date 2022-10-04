e5fda5d070527589358bd5e36bcea54f5cbe6bf9
Milan Zagorac <zagoracm@gmail.com>
2022-10-04 10:53:05 -0400

    start writing tests but these need to be updated/corrected

--- src/tests/App.spec.tsx
+++ src/tests/App.spec.tsx
@@ -1,1 +1,9 @@
-export {};
+import App from "../App";
+import renderer from "react-test-renderer";
+import { render } from "@testing-library/react";
+
+describe("Testing App.tsx", () => {
+  it("Should render without crashing", () => {
+    render(<App />);
+  });
+});
