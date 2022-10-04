4afc7dd0ac1389a951997b0bcacffcb8595f61c4
Milan Zagorac <zagoracm@gmail.com>
2022-10-04 11:05:58 -0400

    removed test files

--- src/tests/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import App from "../App";
-import renderer from "react-test-renderer";
-import { render } from "@testing-library/react";
-
-describe("Testing App.tsx", () => {
-  it("Should render without crashing", () => {
-    render(<App />);
-  });
-});
