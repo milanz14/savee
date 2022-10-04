42827b091f618f6e31221c11e921de111d4e523b
Milan Zagorac <zagoracm@gmail.com>
2022-10-03 20:37:13 -0400

    remove unused React import

--- src/components/AddTransactionForm.tsx
+++ src/components/AddTransactionForm.tsx
@@ -2,7 +2,7 @@
 import { v4 as uuidv4 } from "uuid";
 
 // react imports
-import React, { useState } from "react";
+import { useState } from "react";
 
 // components
 import Button from "./Button";
