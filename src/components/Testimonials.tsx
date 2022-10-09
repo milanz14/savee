d0ec99bc6c13b0c583417d62c94c8a7dd210d727
Milan Zagorac <zagoracm@gmail.com>
2022-10-09 10:02:55 -0400

    about section finished, minor changes to testimonials section

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -3,7 +3,10 @@ import testimonial from "../assets/testimonial4.png";
 
 const Testimonials = (): JSX.Element => {
   return (
-    <section className="text-center bg-info py-5 shadow-box" id="testimonials">
+    <section
+      className="text-center bg-info bg-gradient-to-primary py-5"
+      id="testimonials"
+    >
       <div className="container px-5">
         <div className="row gx-5 justify-content-center">
           <div className="col-xl-8">
@@ -17,8 +20,9 @@ const Testimonials = (): JSX.Element => {
               style={{ height: "5rem" }}
             />
             <p className="mt-4 fs-3 text-white fst-italic">
-              Geoff Harcourt <br />
-              ExpandaBank <br />
+              Geoff H.
+              <br />
+              NewBank Inc <br />
               CEO
             </p>
           </div>
