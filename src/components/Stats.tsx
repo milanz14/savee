b331e5d92cb3fe124cae096e74dc54ff725379ad
Milan Zagorac <zagoracm@gmail.com>
2022-10-09 17:52:20 -0400

    add the CTA component, finished stats section

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,7 +1,42 @@
 import React from "react";
 
 const Stats = (): JSX.Element => {
-  return <div>Stats</div>;
+  const stats = [
+    {
+      id: "stats-1",
+      title: "Active Users",
+      value: "1800+",
+      icon: "fa-solid fa-users px-2",
+    },
+    {
+      id: "stats-3",
+      title: "Transactions",
+      value: "$15MM+",
+      icon: "fa-solid fa-arrow-right-arrow-left px-2",
+    },
+  ];
+
+  return (
+    <section>
+      <div className="container-fluid d-flex justify-content-center align-items-center row py-5 bg-primary bg-gradient">
+        <h2 className="text-center  text-white pb-5">
+          Join one of the most trusted tools used to monitor personal finances.
+        </h2>
+        <div className="row">
+          {stats.map((stat) => (
+            <div className="col-md-6 mb-5">
+              <div className="text-center justify-content-center text-white fs-3">
+                <span>
+                  {stat.value}&nbsp;{stat.title}&nbsp;
+                  <i className={stat.icon}></i>
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Stats;
