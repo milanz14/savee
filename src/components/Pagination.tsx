524c9b93ae03c6e1643ba8b6a8af45aad2feab23
Milan Zagorac <zagoracm@gmail.com>
2023-03-31 14:46:11 -0400

    remove the pagination component and related files:

--- src/components/Pagination.tsx
+++
@@ -1,8 +0,0 @@
-// interfaces
-import { PaginationProps } from "../interfaces/PaginationProps";
-
-const Pagination = (props: PaginationProps) => {
-  return <div>Pagination</div>;
-};
-
-export default Pagination;
