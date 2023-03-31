d3f87870513fbdef182205fb4adde85af3edde48
Milan Zagorac <zagoracm@gmail.com>
2023-03-31 11:08:15 -0400

    remove comments in index.html, add Pagination component and interface --> TODO

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,8 @@
+// interfaces
+import { PaginationProps } from "../interfaces/PaginationProps";
+
+const Pagination = (props: PaginationProps) => {
+  return <div>Pagination</div>;
+};
+
+export default Pagination;
