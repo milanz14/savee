d508c472bacbd7560f51010cced39d977ee68086
Milan Zagorac <zagoracm@gmail.com>
2023-03-31 11:25:40 -0400

    changed order of buttons for better UX flow

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -9,19 +9,10 @@ const Profile = (): JSX.Element => {
 
   const handleLogout = () => {
     logout();
-    // .then((res: any) => {
-    //   setAlerts("Successfully logged out");
-    //   setAlertClass("alert alert-success");
-    //   navigate("/");
-    // })
-    // .catch((err: any) => {
-    //   setAlerts("Unable to log out.");
-    //   setAlertClass("alert alert-danger");
-    // });
     navigate("/");
   };
 
-  const handleDeleteAccount = () => {
+  const handleDeleteAccount = (): void => {
     deleteAccount(currentUser.uid).then(() => {
       console.log("User Deleted");
     });
@@ -38,18 +29,22 @@ const Profile = (): JSX.Element => {
           <p className="mb-2">Name: {currentUser.displayName}</p>
           <p className="mb-2">Email: {currentUser.email}</p>
         </div>
-        <div className="d-flex flex-column flex-md-row mb-2 py-5">
-          <Link to="/update-profile" className="btn btn-primary mr-2 mb-1">
-            Edit
-          </Link>
+        <div className="d-flex flex-column mb-2 py-5">
           <Link to="/dashboard" className="btn btn-info mr-2 mb-1">
             Dashboard
           </Link>
-          <button className="btn btn-warning mr-2 mb-1" onClick={() => handleLogout()}>
-            Logout
-          </button>
-          <button className="btn btn-danger mr-2 mb-1" onClick={() => handleDeleteAccount()}>
+          <Link to="/update-profile" className="btn btn-primary mr-2 mb-1">
+            Edit
+          </Link>
+          <button
+            className="btn btn-danger mr-2 mb-1"
+            onClick={() => handleDeleteAccount()}>
             Delete
+          </button>
+          <button
+            className="btn btn-warning mr-2 mb-1"
+            onClick={() => handleLogout()}>
+            Logout
           </button>
         </div>
       </div>
