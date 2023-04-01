c890b8d7ce35affad9a6c86f47639374fd66a0e3
Milan Zagorac <zagoracm@gmail.com>
2023-04-01 07:46:46 -0400

    color options and use effect on chart

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -2,15 +2,42 @@
 import { ChartDataProps } from "../interfaces/ChartDataProps";
 import { CategorySorted } from "../interfaces/CategorySorted";
 
+// npm imports
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+// react imports
+import { useState, useEffect } from "react";
+
 interface ChartProps {
   data: CategorySorted[];
 }
+
+const colorOptions = [
+  "Red",
+  "Blue",
+  "Green",
+  "Yellow",
+  "Orange",
+  "Cyan",
+  "Magenta",
+  "Pink",
+  "Purple",
+  "Black",
+  "Mauve",
+];
 
 const Chart = ({ data }: ChartProps): JSX.Element => {
   const INITIAL_CHART_DATA = {
     labels: [],
     datasets: [{ label: "", data: 1 }],
   };
+
+  const [chartData, setChartData] = useState(INITIAL_CHART_DATA);
+
+  useEffect(() => {
+    //
+  }, [data]);
 
   return (
     <div>
