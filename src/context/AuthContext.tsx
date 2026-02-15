9e33cf7a3c57eb2c7d126bc6d1d1a2b4ed41ede8
Milan Zagorac <zagoracm@gmail.com>
2026-02-15 18:47:54 -0500

    reodering context and provider

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,43 +1,4 @@
-import { createContext, useEffect, useMemo, useState } from "react";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import type {
-  AuthContextInterface,
-  User,
-  AuthProviderProps,
-} from "../interfaces/interfaces";
+import type { AuthContextInterface } from "../interfaces/interfaces";
+import { createContext } from "react";
 
 export const AuthContext = createContext<AuthContextInterface | null>(null);
-
-export const AuthProvider = ({ children }: AuthProviderProps) => {
-  const [user, setUser] = useState<User | null>(null);
-  const [isLoading, setLoading] = useState<boolean>(true);
-
-  const auth = getAuth();
-
-  const registerWithEmail = () => {};
-  const registerWithGmail = () => {};
-
-  const loginWithEmail = () => {};
-  const loginWithGmail = () => {};
-
-  const logoutEmail = () => {};
-  const logoutGmail = () => {};
-
-  const value = useMemo(
-    () => ({
-      user,
-      isAuthenticated: !!user,
-      registerWithEmail,
-      registerWithGmail,
-      loginWithEmail,
-      loginWithGmail,
-      logoutEmail,
-      logoutGmail,
-    }),
-    [user],
-  );
-
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-};
-
-export default AuthProvider;
