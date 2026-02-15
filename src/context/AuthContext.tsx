ead02aa109b6698245caf0101a44dd0f32325fe2
Milan Zagorac <zagoracm@gmail.com>
2026-02-15 18:49:02 -0500

    move useAuth hook into AuthContext file

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,12 @@
 import type { AuthContextInterface } from "../interfaces/interfaces";
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 export const AuthContext = createContext<AuthContextInterface | null>(null);
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within a AuthProvider");
+  }
+  return context;
+};
