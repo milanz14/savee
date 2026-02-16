1badacb19f945c6a5a44c10e7a389daaf9b528f2
Milan Zagorac <zagoracm@gmail.com>
2026-02-16 00:00:37 -0500

    found and fixed error on register form validation

--- src/auth/Register.tsx
+++ src/auth/Register.tsx
@@ -1,9 +1,7 @@
 import { Button, TextInput } from "@mantine/core";
-// import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { userSchema } from "../lib/validation/validationSchemas";
-import type { z } from "zod";
 import type { RegisterFormValues } from "../interfaces/interfaces";
 
 const Register = () => {
