5ae120e9aff6c46bbd91ee0fb5da5c429ef4ccda
Milan Zagorac <zagoracm@gmail.com>
2026-02-17 14:28:07 -0500

    removed the variables that were no longer needed with the removal of setPersistence

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,9 +1,5 @@
 import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  setPersistence,
-  browserLocalPersistence,
-} from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
