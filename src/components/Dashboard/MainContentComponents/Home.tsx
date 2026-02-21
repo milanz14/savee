d83020e5f148974fd84c6b9ea472f9224f4d46ef
Milan Zagorac <zagoracm@gmail.com>
2026-02-21 12:29:58 -0500

    create responsive grid in Dashboard home page; change label on Add transaction button

--- src/components/Dashboard/MainContentComponents/Home.tsx
+++ src/components/Dashboard/MainContentComponents/Home.tsx
@@ -5,10 +5,10 @@ import Tiles from "./Tiles";
 
 const Home = () => {
   return (
-    <div>
-      <Chart />
+    <div className="grid md:grid-cols-2 md:gap-2 md:grid-rows-2 grid-cols-1 grid-rows-4 gap-2">
       <Performance />
       <Tiles />
+      <Chart />
       <Recent />
     </div>
   );
