c45247c10b2ee1c3950b9b2654cf644889124a08
Milan Zagorac <zagoracm@gmail.com>
2026-03-10 11:57:35 -0400

    removed mantine imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from "@tanstack/react-router";
-import { MantineProvider } from "@mantine/core";
+// import { MantineProvider } from "@mantine/core";
 // import { QueryClientProvider } from "@tanstack/react-query";
 
-import "@mantine/core/styles.css";
+// import "@mantine/core/styles.css";
 
 function App() {
   return (
-    <MantineProvider defaultColorScheme="dark">
-      <Outlet />
-    </MantineProvider>
+    // <MantineProvider defaultColorScheme="dark">
+    //   <Outlet />
+    // </MantineProvider>\
+    <Outlet />
   );
 }
 
