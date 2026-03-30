64467980006d0792ffe66d88703f0372575c7f41
Milan Zagorac <zagoracm@gmail.com>
2026-03-30 17:13:10 -0400

    working on it

--- src/components/TransactionModal/TransactionModalComponents/AddTransaction.tsx
+++ src/components/TransactionModal/TransactionModalComponents/AddTransaction.tsx
@@ -37,15 +37,17 @@ const AddTransactionForm = ({
   );
 
   return (
-    <form onSubmit={handleSubmit(onSubmit, onError)}>
-      <div className="relative my-5">
-        <label htmlFor="amount">Amount</label>
+    <form
+      onSubmit={handleSubmit(onSubmit, onError)}
+      className="w-full flex flex-col min-w-[350px]">
+      <div className="relative my-5 w-full">
+        <label htmlFor="amount">Amount:</label>
         <div className="relative">
           <span className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 pointer-events-none">
             $
           </span>
           <input
-            className="w-full border border-gray-300 rounded pl-7 pr-3 py-2 bg-white text-gray-900"
+            className="border border-gray-300 rounded pl-7 pr-3 py-2 bg-white text-gray-900 w-full"
             placeholder="0"
             type="number"
             id="amount"
@@ -59,9 +61,9 @@ const AddTransactionForm = ({
           </span>
         )}
       </div>
-      <div className="relative my-5">
+      <div className="relative my-5 w-full">
         <div>
-          <label htmlFor="category">Category</label>
+          <label htmlFor="category">Category:</label>
           <select
             className="w-full border border-gray-300 rounded px-3 py-2 bg-white text-gray-900"
             {...register("category")}
@@ -73,6 +75,35 @@ const AddTransactionForm = ({
             ))}
           </select>
         </div>
+        {errors.amount && (
+          <span className="text-[#f87171] absolute text-sm -bottom-6 right-0">
+            {errors.amount.message}
+          </span>
+        )}
+      </div>
+      <div className="relative my-5 w-full">
+        <label htmlFor="date">Date:</label>
+        <input
+          className="w-full border border-gray-300 rounded pl-7 pr-3 py-2 bg-white text-gray-900"
+          placeholder="Date"
+          type="date"
+          id="date"
+          {...register("date")}
+        />
+        {errors.amount && (
+          <span className="text-[#f87171] absolute text-sm -bottom-6 right-0">
+            {errors.amount.message}
+          </span>
+        )}
+      </div>
+      <div className="relative my-5 w-full">
+        <label htmlFor="description">Descrption:</label>
+        <input
+          className="w-full border border-gray-300 rounded pl-7 pr-3 py-2 bg-white text-gray-900  "
+          type="text"
+          id="description"
+          {...register("description")}
+        />
         {errors.amount && (
           <span className="text-[#f87171] absolute text-sm -bottom-6 right-0">
             {errors.amount.message}
