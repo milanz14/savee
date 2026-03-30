6b5f9f3f672a75b94a990f3c03e722283019e5ed
Milan Zagorac <zagoracm@gmail.com>
2026-03-30 11:52:21 -0400

    add h1 and user name display on dashboard

--- src/components/Dashboard/MainContentComponents/Home.tsx
+++ src/components/Dashboard/MainContentComponents/Home.tsx
@@ -1,16 +1,22 @@
+import { useAuth } from "../../../context/AuthContext";
 import Chart from "./Chart";
 import Performance from "./Performance";
 import RecentTransactions from "./RecentTransactions";
 import Tiles from "./Tiles";
 
 const Home = () => {
+  const { user } = useAuth();
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 auto-rows-[350px] gap-2">
-      <Performance />
-      <Tiles />
-      <Chart />
-      <RecentTransactions />
-    </div>
+    <>
+      <h1>Welcome, {user!.displayName}!</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 auto-rows-[375px] gap-2">
+        <Performance />
+        <Tiles />
+        <Chart />
+        <RecentTransactions />
+      </div>
+    </>
   );
 };
 
