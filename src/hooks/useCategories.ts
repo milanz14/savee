489fbbf5743d431c3300155dfc3a9425e8f08873
Milan Zagorac <zagoracm@gmail.com>
2026-03-31 14:09:25 -0400

    update the query hooks to reflect subcollection paths

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs, query } from "firebase/firestore";
 import { db } from "../lib/firebase";
 
 export function useCategories(uid: string | undefined) {
   return useQuery({
     queryKey: ["categories", uid],
     queryFn: async () => {
-      const q = query(collection(db, "categories"), where("uid", "==", uid));
+      const q = query(collection(db, "users", uid!, "categories"));
       const snap = await getDocs(q);
       return snap.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
     },
