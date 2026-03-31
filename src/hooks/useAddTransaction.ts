944ed1d27e1c96194d41b91bd0a7ecb20fe5dd21
Milan Zagorac <zagoracm@gmail.com>
2026-03-31 00:06:40 -0400

    tanstack client query mutation for adding transaction

---
+++ src/hooks/useAddTransaction.ts
@@ -0,0 +1,14 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { addTransaction } from "../lib/mutations/addTransaction";
+import type { TransactionPayload } from "../interfaces/interfaces";
+
+export const useAddTransaction = (uid: string) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (payload: TransactionPayload) => addTransaction(payload),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["transactions", uid] });
+    },
+  });
+};
