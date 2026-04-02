4f40f23b14406abcb9cfee46d8c954f46904f214
Milan Zagorac <zagoracm@gmail.com>
2026-04-02 12:00:05 -0400

    remove erroneous imports

--- src/components/TransactionModal/TransactionModalComponents/AddTransaction.tsx
+++ src/components/TransactionModal/TransactionModalComponents/AddTransaction.tsx
@@ -1,4 +1,4 @@
-import { get, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { transactionSchema } from "../../../lib/validation/validationSchemas";
 import type {
