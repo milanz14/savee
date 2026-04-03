c08d04570281fb614a45b40befc285991b8ef66e
Milan Zagorac <zagoracm@gmail.com>
2026-04-03 12:07:12 -0400

    updated packages, add code for chart

--- src/components/Dashboard/MainContentComponents/Chart.tsx
+++ src/components/Dashboard/MainContentComponents/Chart.tsx
@@ -1,7 +1,17 @@
+import { RechartsDevtools } from "@recharts/devtools";
+import { Line, LineChart } from "recharts";
+
 const Chart = () => {
   return (
     <div className="h-full border border-[#818cf8] rounded-lg shadow-2xl">
-      <p>Chart</p>
+      <LineChart
+        style={{ width: "100%", aspectRatio: 1.618, maxWidth: 600 }}
+        responsive
+        data={data}>
+        <Line dataKey="uv" />
+        <RechartsDevtools />
+      </LineChart>
+      );
     </div>
   );
 };
