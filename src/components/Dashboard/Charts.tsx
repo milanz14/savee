3de25c849c91cbd33ba870d728d41fbe3ebd280d
Milan Zagorac <zagoracm@gmail.com>
2026-04-03 14:56:03 -0400

    chart working now

--- src/components/Dashboard/Charts.tsx
+++ src/components/Dashboard/Charts.tsx
@@ -1,16 +1,26 @@
-import { Line, LineChart } from "recharts";
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
 import type { Transaction } from "../../interfaces/interfaces";
 
 const Chart = ({ transactions }: { transactions: Transaction[] }) => {
   return (
     <div className="h-full border border-[#818cf8] rounded-lg shadow-2xl">
-      <LineChart
-        style={{ width: "100%", aspectRatio: 1.618, maxWidth: 600 }}
-        responsive
-        data={transactions}>
-        <Line dataKey="uv" />
-      </LineChart>
-      );
+      <ResponsiveContainer width="100%">
+        <LineChart data={transactions}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Line type="monotone" dataKey="amount" stroke="#818cf8" dot={false} />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
