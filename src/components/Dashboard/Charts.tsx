0a848c0032851de24348965d07545376c2f00131
Milan Zagorac <zagoracm@gmail.com>
2026-04-03 12:50:57 -0400

    ditch the table rework

--- src/components/Dashboard/Charts.tsx
+++ src/components/Dashboard/Charts.tsx
@@ -1,11 +1,22 @@
-import type { Transaction } from "../../interfaces/interfaces";
+import { Line, LineChart } from "recharts";
+import { useAuth } from "../../context/AuthContext";
+import { useTransactions } from "../../hooks/useTransaction";
 
-const Charts = ({ transactions }: { transactions: Transaction[] }) => {
+const Chart = () => {
+  const { user } = useAuth();
+  const { data: transactions } = useTransactions(user?.uid);
+
   return (
-    <div>
-      <h1>Charts Page</h1>
+    <div className="h-full border border-[#818cf8] rounded-lg shadow-2xl">
+      <LineChart
+        style={{ width: "100%", aspectRatio: 1.618, maxWidth: 600 }}
+        responsive
+        data={transactions}>
+        <Line dataKey="uv" />
+      </LineChart>
+      );
     </div>
   );
 };
 
-export default Charts;
+export default Chart;
