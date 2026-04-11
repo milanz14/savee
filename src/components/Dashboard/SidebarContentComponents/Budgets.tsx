30d09bdbb295d4a98e3be90a33791ac023452cb5
Milan Zagorac <zagoracm@gmail.com>
2026-04-11 13:45:39 -0400

    add comments on the budgets page for what is planned to be done

--- src/components/Dashboard/SidebarContentComponents/Budgets.tsx
+++ src/components/Dashboard/SidebarContentComponents/Budgets.tsx
@@ -1,4 +1,9 @@
 const Budgets = () => {
+  // show the categories as little pills with the category colour.
+  // menu to add a new category, assign a budget and a colour.
+
+  // assign a budget to existing default categories
+
   return (
     <div>
       <h1>Budgets</h1>
