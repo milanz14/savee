ec06a4b7fecbc330b6ad2d0d11b076169c8d0df2
Milan Zagorac <zagoracm@gmail.com>
2026-04-11 13:08:57 -0400

    sidebar content

--- src/components/Dashboard/SidebarContentComponents/Settings.tsx
+++ src/components/Dashboard/SidebarContentComponents/Settings.tsx
@@ -1,9 +1,13 @@
 import { type User } from "firebase/auth";
 
 const Settings = ({ user }: { user: User }) => {
+  console.log(user);
+
   return (
     <div>
       <h1>{user.displayName}'s Settings</h1>
+      <p>Email: {user.email}</p>
+      <p>Email Verified: {user.emailVerified ? "true" : "false"}</p>
     </div>
   );
 };
